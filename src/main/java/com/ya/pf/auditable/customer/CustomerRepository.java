4b674e11e344ebf887a47236e1c004ea1c399d41
Youssef Abadir <joe4ict@gmail.com>
2023-03-31 12:32:15 +0200

    Cleanup

--- src/main/java/com/ya/pf/auditable/customer/CustomerRepository.java
+++ src/main/java/com/ya/pf/auditable/customer/CustomerRepository.java
@@ -4,14 +4,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
 
 	Page<CustomerEntity> findByIsDeletedFalse(Pageable pageable);
 
 	Page<CustomerEntity> findByNameContainingAndIsDeletedFalse(String name, Pageable pageable);
-
-	List<CustomerEntity> findByNameContainingAndIsDeletedFalse(String name);
 
 }
