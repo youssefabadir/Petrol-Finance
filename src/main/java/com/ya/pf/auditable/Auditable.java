b638441a9a24b9adfd716e17382d7c8e3843a1d8
Youssef Abadir <joe4ict@gmail.com>
2023-03-31 12:02:03 +0200

    Implement Auditable.java

---
+++ src/main/java/com/ya/pf/auditable/Auditable.java
@@ -0,0 +1,28 @@
+package com.ya.pf.auditable;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Auditable {
+
+	@CreatedDate
+	@Column(name = "created_date", updatable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date createdDate;
+
+	@LastModifiedDate
+	@Column(name = "last_modified_date")
+	@Temporal(TemporalType.TIMESTAMP)
+	protected Date lastModifiedDate;
+
+}
