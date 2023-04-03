5aff35e3b1d4c5659a5e666edfcb548ae70e1791
Youssef Abadir <joe4ict@gmail.com>
2023-04-03 17:22:38 +0200

    Add missing fields in PaymentDTO

--- src/main/java/com/ya/pf/auditable/payment/dto/PaymentDTO.java
+++ src/main/java/com/ya/pf/auditable/payment/dto/PaymentDTO.java
@@ -1,12 +1,19 @@
 package com.ya.pf.auditable.payment.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.ya.pf.auditable.customer.dto.CustomerDTO;
+import com.ya.pf.auditable.wayofpayment.dto.WayOfPaymentDTO;
+
+import java.util.Date;
 
 public record PaymentDTO(
 		long id,
 		CustomerDTO customer,
+		WayOfPaymentDTO wayOfPaymentDTO,
 		String receiptNumber,
-		double amount
+		double amount,
+		@JsonFormat(pattern = "dd-MM-yyyy")
+		Date paymentDate
 ) {
 
 }
--- src/main/java/com/ya/pf/auditable/payment/dto/PaymentDTOMapper.java
+++ src/main/java/com/ya/pf/auditable/payment/dto/PaymentDTOMapper.java
@@ -2,6 +2,7 @@ package com.ya.pf.auditable.payment.dto;
 
 import com.ya.pf.auditable.customer.dto.CustomerDTOMapper;
 import com.ya.pf.auditable.payment.PaymentEntity;
+import com.ya.pf.auditable.wayofpayment.dto.WayOfPaymentDTOMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,13 +15,17 @@ public class PaymentDTOMapper implements Function<PaymentEntity, PaymentDTO> {
 
 	private final CustomerDTOMapper customerDTOMapper;
 
+	private final WayOfPaymentDTOMapper wayOfPaymentDTOMapper;
+
 	@Override
 	public PaymentDTO apply(PaymentEntity paymentEntity) {
 
 		return new PaymentDTO(paymentEntity.getId(),
 				customerDTOMapper.apply(paymentEntity.getCustomerEntity()),
+				wayOfPaymentDTOMapper.apply(paymentEntity.getWayOfPaymentEntity()),
 				paymentEntity.getReceiptNumber(),
-				paymentEntity.getAmount());
+				paymentEntity.getAmount(),
+				paymentEntity.getPaymentDate());
 	}
 
 }
