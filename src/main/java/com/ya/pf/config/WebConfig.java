64438b58759534b1a2d4d98f3e9caf60e9fc2be9
Youssef Abadir <joe4ict@gmail.com>
2023-05-18 14:23:09 +0300

    Add /api before the endpoints url

---
+++ src/main/java/com/ya/pf/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.ya.pf.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerTypePredicate;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+
+        configurer.addPathPrefix("/api", HandlerTypePredicate.forBasePackage("com.ya.pf.auditable"));
+    }
+
+}
